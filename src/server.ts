4905a1e163f3f7bb3e8aea55b9119d30d9bf0335
pitmov <morph@8b2efe3b4820>
2025-06-27 11:16:10 +0000

    Morph: New Migration

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { createApp } from './app';
 const PORT = process.env.PORT || 3000;
 const app = createApp();
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  (`Server is running on port ${PORT}`);
 });
